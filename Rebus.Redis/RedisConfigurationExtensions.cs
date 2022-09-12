f58120d020f6cb64fdcc1e75df23bc00707fb859
Kirill Bestemyanov <kbestemyanov@acumatica.com>
2022-09-12 13:54:30 +0300

    Some naive implementation of transport

---
+++ Rebus.Redis/RedisConfigurationExtensions.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using Rebus.Config;
+using Rebus.Injection;
+using Rebus.Logging;
+using Rebus.Subscriptions;
+using Rebus.Transport;
+
+namespace Rebus.Redis
+{
+    public static class RedisConfigurationExtensions
+    {
+        private const string RedisSubText = "The Redis transport was inserted as the subscriptions storage because it has native support for pub/sub messaging";
+
+        /// <summary>
+        /// Configures Rebus to use RabbitMQ to move messages around
+        /// </summary>
+        public static RedisOptionsBuilder UseRedis(
+            this StandardConfigurer<ITransport> configurer,
+            string connectionString, 
+            string sectionKey,
+            string inputQueueName)
+        {
+            if (connectionString == null)
+                throw new ArgumentNullException(nameof (connectionString));
+            if (inputQueueName == null)
+                throw new ArgumentNullException(nameof (inputQueueName));
+            return BuildInternal(configurer, false, (Func<IResolutionContext, RedisOptionsBuilder, RedisTransport>) ((context, options) => new RedisTransport(connectionString, sectionKey, inputQueueName, context.Get<IRebusLoggerFactory>())));
+        }
+
+        private static RedisOptionsBuilder BuildInternal(
+            StandardConfigurer<ITransport> configurer,
+            bool oneWay,
+            Func<IResolutionContext, RedisOptionsBuilder, RedisTransport> redisTransportBuilder)
+        {
+            if (configurer == null)
+                throw new ArgumentNullException(nameof (configurer));
+            var options = new RedisOptionsBuilder();
+            configurer.OtherService<RedisTransport>().Register(c =>
+            {
+                var transport = redisTransportBuilder(c, options);
+                options.Configure(transport);
+                return transport;
+            });
+            configurer.OtherService<ISubscriptionStorage>().Register(c => c.Get<RedisTransport>(), "The RabbitMQ transport was inserted as the subscriptions storage because it has native support for pub/sub messaging");
+            configurer.Register(c => c.Get<RedisTransport>());
+            if (oneWay)
+                OneWayClientBackdoor.ConfigureOneWayClient(configurer);
+            return options;
+        }
+    }
+
+    public class RedisOptionsBuilder
+    {
+        public void Configure(RedisTransport transport)
+        {
+            
+        }
+    }
+}
---
+++ Rebus.Redis/RedisTransport.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Rebus.Exceptions;
+using Rebus.Logging;
+using Rebus.Messages;
+using Rebus.Subscriptions;
+using Rebus.Transport;
+using StackExchange.Redis;
+
+namespace Rebus.Redis
+{
+    public class RedisTransport:AbstractRebusTransport, ISubscriptionStorage
+    {
+        private readonly string _sectionKey;
+        private readonly ConnectionMultiplexer _connectionMultiplexer;
+        private readonly HashSet<string> _registeredSubscriptions = new HashSet<string>();
+        private readonly SemaphoreSlim _subscriptionSemaphore = new SemaphoreSlim(1, 1);
+        private readonly ILog _log;
+        private const string TopicExchangeName = "RebusTopics";
+        private const int WaitTimeout = 2000;
+        private readonly BlockingCollection<(string topic, RedisValue value)> _messagesToProcess = new BlockingCollection<(string, RedisValue v)>();
+
+        public RedisTransport(string connectionString, string sectionKey, 
+            string inputQueueAddress,
+            IRebusLoggerFactory rebusLoggerFactory) : base(inputQueueAddress)
+        {
+            _sectionKey = sectionKey??"";
+            _connectionMultiplexer = ConnectionMultiplexer.Connect(connectionString);
+            _log = rebusLoggerFactory.GetLogger<RedisTransport>();
+        }
+
+        public override void CreateQueue(string address)
+        {
+            var topic = EnsureFullyQualifiedName(address);
+            _connectionMultiplexer.GetSubscriber().Subscribe(topic, CommonSubscriber);
+        }
+
+        public override async Task<TransportMessage> Receive(ITransactionContext context, CancellationToken cancellationToken)
+        {
+            (string topic, RedisValue value) serializedMessage = default;
+            try
+            {
+                _log.Debug("Waiting for message arrive");
+                if (!_messagesToProcess.TryTake(out serializedMessage, WaitTimeout, cancellationToken))
+                {
+                    _log.Debug("No messages within timeout {0} ms", WaitTimeout);
+                    return null;
+                }
+
+                _log.Debug("Message received");
+            }
+            catch (OperationCanceledException ex)
+            {
+                _log.Debug("Reading from queue was cancelled");
+                return (TransportMessage) null;
+            }
+            catch(Exception ex)
+            {
+                throw new RebusApplicationException(ex,
+                    "Unexpected exception thrown while trying to dequeue Redis Bus");
+            }
+            if (serializedMessage == default)
+                return null;
+            try
+            {
+                return JsonConvert.DeserializeObject<TransportMessage>(serializedMessage.value);
+            }
+            catch (Exception ex)
+            {
+                throw new RebusApplicationException(ex,
+                    "Unexpected exception thrown while trying to deserialize Redis message");
+            }
+        }
+
+        protected override async Task SendOutgoingMessages(IEnumerable<OutgoingMessage> outgoingMessages, ITransactionContext context)
+        {
+            foreach (var outgoingMessage in outgoingMessages)
+            {
+                var serializedMessage = JsonConvert.SerializeObject(outgoingMessage.TransportMessage);
+                await _connectionMultiplexer.GetSubscriber()
+                    .PublishAsync(outgoingMessage.DestinationAddress, serializedMessage);
+            }
+        }
+
+        public Task<string[]> GetSubscriberAddresses(string topic)
+        {
+            topic = EnsureFullyQualifiedName(topic);
+            return Task.FromResult(new[]{ topic });
+        }
+
+        private string EnsureFullyQualifiedName(string topic)
+        {
+            if (!topic.Contains("@"))
+                topic = topic + "@" + _sectionKey + "_" + TopicExchangeName;
+            return topic;
+        }
+
+        public async Task RegisterSubscriber(string topic, string subscriberAddress)
+        {
+            var fullyQualifiedTopic = EnsureFullyQualifiedName(topic);
+            _log.Debug("Registering subscriber {subscription}", (fullyQualifiedTopic, subscriberAddress));
+            await this._subscriptionSemaphore.WaitAsync();
+            try
+            {
+                if (!_registeredSubscriptions.Contains(fullyQualifiedTopic))
+                {
+                    await _connectionMultiplexer.GetSubscriber().SubscribeAsync(fullyQualifiedTopic,
+                        CommonSubscriber);
+                    _registeredSubscriptions.Add(fullyQualifiedTopic);
+
+                }
+            }
+            finally
+            {
+                this._subscriptionSemaphore.Release();
+            }
+
+        }
+
+        private void CommonSubscriber(RedisChannel c, RedisValue v)
+        {
+            _messagesToProcess.Add((c.ToString(), v));
+        }
+
+        public async Task UnregisterSubscriber(string topic, string subscriberAddress)
+        {
+            var fullyQualifiedTopic = EnsureFullyQualifiedName(topic);
+            _log.Debug("Unregistering subscriber {subscription}", (fullyQualifiedTopic, subscriberAddress));
+            await this._subscriptionSemaphore.WaitAsync();
+            try
+            {
+                if (_registeredSubscriptions.Contains(fullyQualifiedTopic))
+                {
+                    await _connectionMultiplexer.GetSubscriber().UnsubscribeAsync(fullyQualifiedTopic);
+                    _registeredSubscriptions.Remove(fullyQualifiedTopic);
+                }
+            }
+            finally
+            {
+                this._subscriptionSemaphore.Release();
+            }
+        }
+
+        public bool IsCentralized => true;
+    }
+}
